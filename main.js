7a13fef5ce4c657c0849e925129d503bb8be1772
Akash kumar <akash8825317696@gmail.com>
2023-02-03 23:29:01 +0530

    commit 4

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function randomHands(){
     
     let randomIndex = Math.floor(Math.random() * hands.length);
     return hands[randomIndex];
-     
+
  };
 
 const randomP1 = document.querySelector('.random-p1')
@@ -15,53 +15,69 @@ const randomP2 = document.querySelector('.random-p2')
 const start1 = document.querySelector('.start1')
 const start2 = document.querySelector('.start2')
 
-playBtn.addEventListener('click',function(e){
-    let target = e.target
+const imgBtns = document.querySelectorAll('.img-btns')
+const img = document.querySelector('.img')
+
+imgBtns.forEach((imgBtns)=>{
+imgBtns.addEventListener('click',function(e){
+    
+    const target = e.currentTarget.dataset.id;
+    console.log(target)
     target.innerText = 'Play Again'
     start1.style.visibility = "visible"
     start2.style.visibility = "visible"
-    let player1Hand = randomHands();
-    let player2Hand = randomHands();
+    let player1Hand = randomHands();//CPU
+    let player2Hand = target;//YOU
+    // console.log(player2Hand)
+    const again = document.querySelector('.again')
+    const score = document.querySelector('.score')
+    
 
     if(player1Hand === player2Hand){
         result.innerHTML = "Draw!"
         randomP1.innerHTML = player1Hand
-        randomP2.innerHTML = player2Hand
+        
+        // randomP2.innerHTML = player2Hand
         
     }else if(player1Hand === 'Rock' && player2Hand === 'Paper'){
-        result.innerHTML = "Player-2 Wins"
+        result.innerHTML = "YOU Wins"
         randomP1.innerHTML = player1Hand
-        randomP2.innerHTML = player2Hand
+        
+        // randomP2.innerHTML = player2Hand
          
     }else if(player1Hand === 'Paper' && player2Hand === 'Scissor'){
-        result.innerHTML = "Player-2 Wins"
+        result.innerHTML = "YOU Wins"
         randomP1.innerHTML = player1Hand
-        randomP2.innerHTML = player2Hand
+        
+        // randomP2.innerHTML = player2Hand
 
     }else if(player1Hand === 'Scissor' && player2Hand === 'Rock'){
-        result.innerHTML = "Player-1 Wins"
+        result.innerHTML = "YOU Wins"
         randomP1.innerHTML = player1Hand
-        randomP2.innerHTML = player2Hand
-
+        // randomP2.innerHTML = player2Hand
+        
 
     }else if(player1Hand === 'Paper' && player2Hand === 'Rock'){
-        result.innerHTML = "Player-1 Wins" 
+        result.innerHTML = "CPU Wins" 
         randomP1.innerHTML = player1Hand
-        randomP2.innerHTML = player2Hand
-
+        // randomP2.innerHTML = player2Hand
+        
 
     }else if(player1Hand === 'Scissor' && player2Hand === 'Paper'){
-        result.innerHTML = "Player-1 Wins"
+        result.innerHTML = "CPU Wins"
         randomP1.innerHTML = player1Hand
-        randomP2.innerHTML = player2Hand
-
+        // randomP2.innerHTML = player2Hand
+        
 
     }else if(player1Hand === 'Rock' && player2Hand === 'Scissor'){
-        result.innerHTML = "Player-2 Wins"
+        result.innerHTML = "CPU Wins"
         randomP1.innerHTML = player1Hand
-        randomP2.innerHTML = player2Hand
-
+        // randomP2.innerHTML = player2Hand
+        
 
     }
 
+    again.innerText = 'Play Again!'
+    
+});
 });
